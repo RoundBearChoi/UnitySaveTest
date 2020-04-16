1da901351db0689910f9afad8951804d93477587
RoundBearChoi <roundbeargames@gmail.com>
2020-04-16 12:23:03 +0900

    choi

---
+++ verysimpletest/Assets/DebugManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DebugManager : Singleton<DebugManager>
+{
+    readonly bool savelog = true;
+
+    public string path;
+    StreamWriter sw;
+
+    private void Awake()
+    {
+        path = Application.persistentDataPath + Path.DirectorySeparatorChar + "UnityConsoleLog.txt";
+        File.WriteAllText(path, System.DateTime.Now.ToString() + "\n\n");
+    }
+
+    public void Log(object message)
+    {
+        if (savelog)
+        {
+            sw = File.AppendText(path);
+            sw.WriteLine(message);
+            sw.Close();
+        }
+
+        Debug.Log(message);
+    }
+}
---
+++ verysimpletest/Assets/SimpleTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTest : MonoBehaviour
+{
+    public UnityEngine.UI.Text uitext1;
+    public UnityEngine.UI.Text uitext2;
+    int simpleint = 0;
+
+    IEnumerator Start()
+    {
+        DebugManager.Instance.Log("application started");
+
+        uitext1.text = DebugManager.Instance.path;
+
+        while(true)
+        {
+            yield return new WaitForSeconds(1f);
+            RaiseNumber();
+        }
+    }
+
+    void RaiseNumber()
+    {
+        simpleint++;
+        uitext2.text = simpleint.ToString();
+    }
+}
